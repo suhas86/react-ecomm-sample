379815ab7255e31c0a10af79f81ce2dc021a562f
Suhas S Murthy <murthy.suhas@gmail.com>
2020-09-15 01:32:24 +0530

    Updates

---
+++ precache-manifest.4bb1ace8be3ccc2089288c0962c8ff9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc266b5505d838bfa93d372b70aca30",
+    "url": "/react-ecomm-sample/index.html"
+  },
+  {
+    "revision": "10d6c0a928bc81340025",
+    "url": "/react-ecomm-sample/static/css/main.c35e003f.chunk.css"
+  },
+  {
+    "revision": "20ea9a996da7694278d7",
+    "url": "/react-ecomm-sample/static/js/2.88956b18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ecomm-sample/static/js/2.88956b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d6c0a928bc81340025",
+    "url": "/react-ecomm-sample/static/js/main.9f5cf016.chunk.js"
+  },
+  {
+    "revision": "997a53edebca417197e6",
+    "url": "/react-ecomm-sample/static/js/runtime-main.e253932b.js"
+  },
+  {
+    "revision": "e4072dc0ea58c0cfbe4f4a0ad941b501",
+    "url": "/react-ecomm-sample/static/media/banner.e4072dc0.jpg"
+  }
+]);
